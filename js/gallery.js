642871c5ff0003ce1b4639233e9299c820337788
Myroslava <m6i6y6a@gmail.com>
2025-07-26 13:53:53 +0200

    add js, css, modal w

--- js/gallery.js
+++ js/gallery.js
@@ -63,3 +63,44 @@ const images = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+    })
+    .join('');
+}
+
+galleryContainer.innerHTML = createGalleryMarkup(images);
+
+galleryContainer.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  const isImageEl = event.target.classList.contains('gallery-image');
+  if (!isImageEl) {
+    return;
+  }
+  const largeImageURL = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="1112" height="640">
+  `);
+
+  instance.show();
+}
